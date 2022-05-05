d5c667946a5bd4e5bb3f11877858e8680107e972
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-05-05 17:45:29 +1000

    able to add and view nfts

---
+++ pages/my-assets.js
@@ -0,0 +1,15 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from 'web3modal'
+
+import {
+  nftmarketaddress, nftaddress
+} from '../config'
+
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
+
+export default function MyAssets() {
+  
+}
